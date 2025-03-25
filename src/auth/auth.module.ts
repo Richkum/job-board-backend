9ddd06c18efcae380052a4a93aca0cca2af80f4e
Richkum <kumatum2@gmail.com>
2025-03-25 17:14:49 +0100

    refactor: Remove commented out code and unused imports in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,15 +4,15 @@ import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtStrategy } from './gaurd/jwt.strategy'; // Adjust the path if needed
-import { JwtAuthGuard } from './gaurd/jwt-auth.guard'; // Optional, you'll use it in controllers as needed
+import { JwtStrategy } from './gaurd/jwt.strategy';
+import { JwtAuthGuard } from './gaurd/jwt-auth.guard';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: process.env.JWT_SECRET || 'yourSecretKey', // Set your JWT_SECRET in your .env file ideally
-      signOptions: { expiresIn: '1d' }, // Token expiry time
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' }, // Token expiry time
     }),
   ],
   providers: [AuthService, JwtStrategy],
