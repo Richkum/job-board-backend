ff557870904fa9faa124bdd2b0f45635411507f3
Richkum <kumatum2@gmail.com>
2025-03-26 14:18:00 +0100

    refactor: Update dependencies and middleware for device detection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,3 @@
-// import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { MongooseConfigService } from './config/mongoose.config';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { DeviceMiddleware } from './middleware/device.middleware';
-
-// @Module({
-//   imports: [
-//     MongooseModule.forRootAsync({
-//       useClass: MongooseConfigService,
-//     }),
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {
-//   configure(consumer: MiddlewareConsumer) {
-//     consumer
-//       .apply(DeviceMiddleware)
-//       .forRoutes({ path: 'auth/login', method: RequestMethod.POST });
-//   }
-// }
-
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
