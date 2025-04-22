befcaf457ee1e7d449133c4b01584e63d07718c2
Richkum <kumatum2@gmail.com>
2025-04-22 16:30:43 +0100

    Refactor UsersModule imports and add module descriptions

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,10 +7,26 @@ import { UsersService } from './users.service';
 import { CloudinaryModule } from '../cloudinary/cloudinary.module';
 import { AuthModule } from 'src/auth/auth.module';
 
+/**
+ * UsersModule
+ *
+ * This module contains the user schema, service, and controller
+ */
 @Module({
   imports: [
+    /**
+     * AuthModule is imported to allow use of the JWT authentication
+     * strategy for the users controller
+     */
     AuthModule,
+    /**
+     * MongooseModule is imported to access the User schema
+     */
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    /**
+     * CloudinaryModule is imported to allow use of the cloudinary service
+     * for image uploads
+     */
     CloudinaryModule,
   ],
   controllers: [UsersController],
